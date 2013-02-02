7668d6f75682bf282b986a10d592ae6e135e0324
Thomas Britton <tmbritton@Macbook-of-Awesome.local>
2013-02-02 16:09:40 -0600

    corrected image paths

--- index.php
+++ index.php
@@ -1,10 +1,101 @@
-<?php include('header.php'); ?>
+<?php include('includes/header.php'); ?>
 	<body>
-        	<!--[if lt IE 7]>
-            	<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        	<![endif]-->
+		<nav class="sitetop">
+			<ul>
+				<li><a href="#">Store</a></li>
+				<li><a href="#">Mac</a></li>
+				<li><a href="#">iPod</a></li>
+				<li><a href="#">iPhone</a></li>
+				<li><a href="#">iTunes</a></li>
+				<li><a href="#">Support</a></li>
+			</ul>
+		</nav>
 
-        	<!-- Add your site or application content here -->
-        	<p>Hello world! This is HTML5 Boilerplate.</p>
+		<header>
+			<h1>Example Store</h1>
+			<form method="POST" class="search">
+				<input type="text" name="searchtext">
+				<input type="submit" value="submit">
+			</form>
+			<nav class="sectionnav">
+				<ul>
+					<li><a href="#">Store</a></li>
+					<li><a href="#">Mac</a></li>
+					<li><a href="#">iPod</a></li>
+					<li><a href="#">iPhone</a></li>
+					<li><a href="#">iPad</a></li>
+					<li><a href="#">iTunes</a></li>
+					<li><a href="#">Support</a></li>
+				</ul>
+			</nav>
+		</header>
 
-<?php include('footer.php'); ?>
+		<section class="main">
+			<article class="featured">
+				<h1>Featured Products</h1>
+				<div>
+					<img src="img/large/ipod.jpg" alt="iPod Nano" width="268" height="268" />
+					<h2>iPod Nano</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed condimentum aliquet felis, a facilisis tortor elementum quis. Proin congue, erat vitae sodales fringilla, ipsum dolor interdum eros, sit amet dapibus magna lorem eget orci. Sed nunc quam, posuere eu faucibus eget, egestas vitae augue. Cras tempor congue suscipit. Sed eget cursus eros. Phasellus pharetra mi eu eros cursus pharetra vel id erat. Aenean gravida sem non magna luctus et malesuada libero auctor. Curabitur accumsan libero in purus cursus tristique egestas justo tristique. Nunc rhoncus volutpat arcu sit amet ultricies.</p>
+				</div>
+			</article>
+
+			<article class="trio">
+				<h1>Featured Products</h1>
+				<ul>
+					<li>
+						<img src="img/large/macbook.jpg" alt="MacBook Pro" />
+						<a href="#">[Sample Product] MacBook Pro</a>
+						<p class="price">$1,999.00</p>			
+						<span class="stars three"></span>	
+						<button class="add">Add To Cart</button>
+					</li>
+					<li>
+						<img src="img/large/bag.jpg" alt="Crumpler Considerable Embarrassment Bag" />
+						<a href="#">[Sample Product] Crumpler Considerable Embarrassment Bag</a>
+						<p class="price">$89.95</p>
+						<span class="stars four"></span>
+						<button class="add">Choose Options</button>
+					</li>
+					<li>
+						<img src="img/large/ipod.jpg" alt="iPod Nano" />
+						<a href="#">[Sample Product] iPod Nano</a>
+						<p class="price">$149.00</p>
+						<span class="stars three"></span>
+						<button class="add">Choose Options</button>
+					</li>
+				</ul>
+			</article>
+		</section>	
+
+		<section class="sidebar">
+			<article class="categories">
+				<h1>Categories</h1>
+				<ul>
+					<li><a href="#">iPhone</a></li>
+					<li><a href="#">iPhone 3G</a></li>
+					<li><a href="#">iPhone 3GS</a></li>
+					<li><a href="#">iPhone 4</a></li>
+				</ul>
+			</article>
+
+			<article class="recent">
+				<h1>Recent Products</h1>
+				<ul>
+					<li>
+						<img src="img/thumbs/ipod.jpg" alt="Macbook Pro" />
+						<p>Macbook Pro<br /><span class="price">$1,999.00</span></p>
+					</li>
+					<li>
+						<img src="img/thumbs/ipod.jpg" alt="iPod Nano" />
+						<p>iPod Nano<br /><span class="price">$149.00</span></p>
+					</li>
+					<li>
+						<img src-"img/thumbs/ipod.jpg" alt="Crumpler Considerable Embarrasment Bag" />
+						<p>Crumpler Considerable Embarrasment Bag<br /><span class="price">$89.95</span></p>
+					</li>
+				</ul>
+			</article>
+		</section>
+
+<?php include('includes/footer.php'); ?>
