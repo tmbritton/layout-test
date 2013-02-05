a4acc6df4b1463adf94762d2fde57e513779e15e
Tom Britton <britton.tm@gmail.com>
2013-02-04 18:50:21 -0600

    removed included and switched index to html instead of php for github pages

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+$(document).ready(function(){
+	$('.rating span').click(function(){
+		setRating($(this));
+		//displayRatings();
+	});
+});
 
+var setRating = function(star) {
+	var rating = star.attr('class');
+	$(star).parent('.rating').removeClass('one two three four five');
+	$(star).parent('.rating').addClass(rating);
+}
+
+var displayRatings = function() {
+	$('.rating').each(function(){
+		if($(this).hasClass('one two three four five')){
+			console.log($(this).attr('class'));
+		}
+	});
+
+}
