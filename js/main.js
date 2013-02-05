8259fb9bf242708da5e479d738a24e2c92063b12
Tom Britton <britton.tm@gmail.com>
2013-02-04 21:10:01 -0600

    removed jasvascript 100% fix, adjusted form width to 185px

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	resizeMaxWidth('.sitetop'); 	
+	//resizeMaxWidth('.sitetop'); 	
 });
 
 /**
 * Function resizes elements that are supposed to be 100% of window to
 * window width + scroll width
+*
+*	document.defaultView.scrollMaxX appears to be a FF property only
 *
 * @param string selector
 *		selector to resize
